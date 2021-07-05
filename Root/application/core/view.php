a0e572d7c501dbb01c513024d05b493a6e34b6a7
Sofia Shperling <sonia.shperling@yandex.ru>
2021-07-05 14:14:48 +0700

    index.php,view.php deleted unnecessary comments

--- Root/application/core/view.php
+++ Root/application/core/view.php
@@ -1,12 +1,8 @@
 <?php
 
-//this file for form a site view
 class View
 {
     //public $template_view; /
-//$content_file — виды отображающие контент страниц;
-//$template_file — общий для всех страниц шаблон;
-//$data — массив, содержащий элементы контента страницы. Обычно заполняется в модели.
 
     function generate($content_view, $template_view, $data = null)
     {
--- Root/public/index.php
+++ Root/public/index.php
@@ -1,33 +1,5 @@
 <?php
-//require_once 'header.php';
 
-//    $uri = $_SERVER['REQUEST_URI'];
-//
-//    if ('/' === $uri ){
-//        require 'mainPage.php';
-//    } elseif ('/posts' === $uri){
-//        require 'posts.php';
-//    } elseif ('/authors' === $uri){
-//        require 'authors.php';
-//    }
-
-//switch ($_SERVER['REQUEST_URI']) {
-//    case "/" :
-//        require_once("mainPage.php"); // страница "О Нас"
-//        break;
-//    case "/posts" :
-//        require_once("posts.php"); // страница "Контакты"
-//        break;
-//    case "/authors" :
-//        require_once("authors.php"); // страница "Обратная связь"
-//        break;
-//    default :
-//        require_once("page404.php"); // страница "404"
-//        break;
-//}
-//
-//
-//require_once 'footer.php';
 print_r(1);
 ini_set('display_errors', 1);
 require_once '../application/bootstrap.php';
