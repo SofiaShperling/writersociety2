28901d3eacf038dd4468f696b887967f0f11f9a2
Sofia Shperling <sonia.shperling@yandex.ru>
2021-07-05 14:22:05 +0700

    core/rout.php deleted unnesessery coments

--- Root/application/core/route.php
+++ Root/application/core/route.php
@@ -4,28 +4,22 @@ class Route
 {
     static function start()
     {
-// controller and default action
         $controller_name = 'Main';
         $action_name = 'index';
 
         $routes = explode('/', $_SERVER['REQUEST_URI']);
 
-// get controller name
         if (!empty($routes[1])) {
             $controller_name = $routes[1];
         }
 
-// get action name
         if (!empty($routes[2])) {
             $action_name = $routes[2];
         }
 
-// add prefix
         $model_name = 'Model_' . $controller_name;
         $controller_name = 'Controller_' . $controller_name;
         $action_name = 'action_' . $action_name;
-
-// get file with a model class if it is exist
 
         $model_file = strtolower($model_name) . '.php';
         $model_path = "../application/models/" . $model_file;
@@ -33,7 +27,6 @@ class Route
             include $model_path;
         }
 
-// add file with a controller class
         $controller_file = strtolower($controller_name) . '.php';
         $controller_path = "../application/controllers/" . $controller_file;
         var_dump(file_exists($controller_path));
@@ -43,12 +36,10 @@ class Route
             Route::ErrorPage404();
         }
 
-// create new controller
         $controller = new $controller_name;
         $action = $action_name;
 
         if (method_exists($controller, $action)) {
-// add controller action
             $controller->$action();
         } else {
             Route::ErrorPage404();
