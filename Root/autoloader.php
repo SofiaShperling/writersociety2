d0e988e73528c124e7ea19388f7861c71293071d
Sofia Shperling <sonia.shperling@yandex.ru>
2021-07-08 16:57:22 +0700

    autoloader.php add autoloader

--- Root/autoloader.php
+++ Root/autoloader.php
@@ -1,1 +1,13 @@
 <?php
+namespace Psr4AutoloaderClass;
+
+class Psr4AutoloaderClass{
+
+    public function register()
+    {
+        spl_autoload_register(function ($name) {
+            $name = '../' .str_replace('\\', DIRECTORY_SEPARATOR, $name) . '.php';
+            include $name;
+        });
+    }
+}
