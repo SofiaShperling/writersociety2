6c2a09bce26c366611424f3d7d71964c5ede9fef
Sofia Shperling <sonia.shperling@yandex.ru>
2021-07-28 16:16:24 +0700

    changeda stuture of generate function

--- Root/application/views/View.php
+++ Root/application/views/View.php
@@ -3,12 +3,8 @@ namespace application\views;
 
 class View
 {
-//    private $TemplateHeaderView = 'header.php';
-
-    function generate($ContentView, $TemplateHeaderView, $TemplateFooterView, $data = null)
+    function generate($ContentView, $TemplateView, $data = null)
     {
-        include $TemplateHeaderView;
-        include $ContentView;
-        include $TemplateFooterView;
+        include $TemplateView;
     }
 }
