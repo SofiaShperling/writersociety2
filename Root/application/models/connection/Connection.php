11fa04f4ef177d0423b7a9815b1052c57e34ccac
Sofia Shperling <sonia.shperling@yandex.ru>
2021-07-28 17:40:00 +0700

    changed to PDO connection

--- Root/application/models/connection/Connection.php
+++ Root/application/models/connection/Connection.php
@@ -1,19 +1,20 @@
 <?php
 namespace application\models\connection;
+use PDO;
 
 class Connection
 {
     public function CreateConnection()
     {
-//        $link = mysqli_connect('localhost','root', 'root', 'my_firs_base');
         $user = 'root';
         $pass = 'root';
         $dsn = 'mysql:host=localhost;dbname=my_firs_base';
-        $link = new PDO($dsn, $user, $pass);
-        if (mysqli_connect_errno()) {
-            echo 'Error to connect to database (' . mysqli_connect_errno() . '): ' . mysqli_connect_error();
-            exit();
+        try {
+            $link = new PDO($dsn, $user, $pass);
+        } catch (PDOException $e) {
+            exit('Datebase error.');
         }
+
         return ($link);
     }
 }
