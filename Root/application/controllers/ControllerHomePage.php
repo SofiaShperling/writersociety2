5fd04f9c5953514be7680a250fe579af08d13b7c
Sofia Shperling <sonia.shperling@yandex.ru>
2021-07-28 17:33:17 +0700

    changed ActonMain according to new generate function version

--- Root/application/controllers/ControllerHomePage.php
+++ Root/application/controllers/ControllerHomePage.php
@@ -4,7 +4,8 @@ namespace application\controllers;
 
 use application\controllers\Controller;
 use application\models\ModelCategories;
-use applications\views\View;
+use application\models\ModelGetPosts;
+use application\views\View;
 use application\models\connection\Connection;
 
 class ControllerHomePage extends Controller
@@ -12,11 +13,11 @@ class ControllerHomePage extends Controller
     function __construct()
     {
         $this->model = new ModelCategories();
-//        $this->view = new View();
+        $this->view = new View();
     }
     function ActionMain()
     {
         $data = $this->model->GetCategories();
-        $this->view->generate('MainView.php', 'header.php', 'footer.php', $data);
+        $this->view->generate(__DIR__. '/../views/MainView.php', __DIR__. '/../views/TemplateView.php', $data);
     }
 }
