51d223d76b17b3c7309dce6331904a5cf3640a50
Sofia Shperling <sonia.shperling@yandex.ru>
2021-07-28 16:17:39 +0700

    now template call content view

--- Root/application/views/TemplateView.php
+++ Root/application/views/TemplateView.php
@@ -6,10 +6,33 @@
     <meta name="description" content=""/>
     <link href=".../css/style.css" rel="stylesheet" type="text/css" media="screen"/>
 </head>
-
 <body>
-<?php include 'header.php'; ?>
-<?php //connection 'Root/application/views/' . $ContentView; ?>
-<?php //connection 'footer.php'; ?>
+<div id="logo">
+    <h1><a href="/">Writer Society</a></h1>
+</div>
+<div id="header-wrapper">
+    <div id="header">
+        <div id="menu">
+            <ul>
+                <?php foreach ($data as $category):
+                    $categorylink = $category["title"];
+                    if ($category["title"] == 'main'){
+                        $categorylink = '';
+                    }
+                    ?>
+                    <li><a href="/<?=$categorylink?>"><?=$category["title"]?></a></li>
+                <?php endforeach; ?>
+                <li class="current_page_item"><a href="#"> log in / check in </a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<?php include $ContentView; ?>
+<footer>
+    <div id="footer">
+        <p>Сайт, где пользователи могут делиться своим произведениями, читать других авторов и обсуждать посты в комментариях.
+            since 2021</p>
+    </div>
+</footer>
 </body>
 </html>>
